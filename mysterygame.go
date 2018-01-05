d71d0c99d5e917b93a6013c2c49519ee5b93c568
Erik Gerrits <ghrgerrits@gmail.com>
2018-01-05 22:26:51 +0100

    Added homepage and a way to manage it temporarily using a json file.

--- mysterygame.go
+++ mysterygame.go
@@ -1,19 +1,46 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
 	"os"
 )
 
+type Page struct {
+	Name  string
+	Title string
+}
+
+func page(name string) Page {
+	data, _ := ioutil.ReadFile("data/pages.json")
+
+	var pages []Page
+
+	err := json.Unmarshal(data, &pages)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var page Page
+
+	for _, value := range pages {
+		if value.Name == name {
+			page = value
+		}
+	}
+
+	return page
+}
+
 func handler(writer http.ResponseWriter, request *http.Request) {
 	mysterygameTemplate, _ := template.ParseFiles("templates/mysterygame.html")
 
-	var data = map[string]string{
-		"title": "Mystery Game",
-	}
+	home := page("home")
 
-	mysterygameTemplate.Execute(writer, data)
+	mysterygameTemplate.Execute(writer, home)
 }
 
 func main() {
